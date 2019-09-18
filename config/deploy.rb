5044ff1b2979ba4a2ca739f139ab7e1abef65adc
TakkAlGhul <takuro.mike@gmail.com>
2019-09-18 14:38:56 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ lock "~> 3.11.0"
 set :application, 'TripCo'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'git@github.com:TakkAlGhul/TripCo.git'
+set :repo_url, 'git@github.com:TakkAlGhul/TripCo.git'
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 set :linked_files, %w{ config/secrets.yml }
