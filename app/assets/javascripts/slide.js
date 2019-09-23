f86ecaed4171156c616fe43dc6c354d3aa8bcff0
TakkAlGhul <takuro.mike@gmail.com>
2019-09-24 00:03:27 +0900

    update

--- app/assets/javascripts/slide.js
+++ app/assets/javascripts/slide.js
@@ -18,25 +18,25 @@ $(function(){
 });
 $(function() {
   $('.mid-wrapper__images').slick({
-        infinite: true,
-        dots:false,
-        slidesToShow: 4,
-        slidesToScroll: 4,
-        autoplay: true,
-        autoplaySpeed: 4000,
-        responsive: [{
-             breakpoint: 768,
-                  settings: {
-                       slidesToShow: 2,
-                       slidesToScroll: 2,
-             }
-        },{
-             breakpoint: 480,
-                  settings: {
-                       slidesToShow: 1,
-                       slidesToScroll: 1,
-                  }
-             }
-        ]
+		infinite: true,
+		dots:false,
+		slidesToShow: 4,
+		slidesToScroll: 4,
+		autoplay: true,
+		autoplaySpeed: 4000,
+		responsive: [{
+			breakpoint: 768,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 2,
+					}
+			},{
+				breakpoint: 480,
+					settings: {
+						slidesToShow: 1,
+						slidesToScroll: 1,
+					}
+				}
+			]
    });
 });
