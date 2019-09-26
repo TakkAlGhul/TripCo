45e1f9cc412bba27914dd91dbdccf6dac74e375a
TakkAlGhul <takuro.mike@gmail.com>
2019-09-26 22:14:58 +0900

    valid fix

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -4,7 +4,7 @@ class Plan < ApplicationRecord
   accepts_nested_attributes_for :contents, allow_destroy: true
 
   validates :title, :user_id, presence: true
-
+ 
   def self.search(search)
     return Plan.all unless search
     Plan.where(['title LIKE ?', "%#{search}%"])
--- spec/factories/contents.rb
+++ spec/factories/contents.rb
@@ -4,7 +4,7 @@ FactoryBot.define do
     place_name         { Faker::Name.name }
     description        { Faker::Lorem.sentence }
     plan_id            { Plan.new(id: 1) }
-    image              {File.open("#{Rails.root}/spec/images/289CAEA9-8DA7-45A1-917A-EFFD0FDFF294.jpg")}
+    image              { File.open("#{Rails.root}/spec/images/289CAEA9-8DA7-45A1-917A-EFFD0FDFF294.jpg")}
     time               { "10:00" }
   end
 end
